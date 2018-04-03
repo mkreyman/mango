cdebb3058bcec5f345a9c0c0a1fa1beeed49ba5e
mkreyman <mark@kreyman.com>
2018-04-03 16:57:36 -0600

    Added categories

---
+++ lib/mango_web/views/category_view.ex
@@ -0,0 +1,11 @@
+defmodule MangoWeb.CategoryView do
+  use MangoWeb, :view
+
+  def title_case(string) do
+    string
+    |> String.downcase
+    |> String.split
+    |> Enum.map(&String.capitalize/1)
+    |> Enum.join(" ")
+  end
+end
