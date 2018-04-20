8cf8f5c3ef21db91fdd765f752a0dc516ad52470
mkreyman <mark@kreyman.com>
2018-04-20 16:06:40 -0600

    User admin interface added

--- lib/mango_web/router.ex
+++ lib/mango_web/router.ex
@@ -19,6 +19,10 @@ defmodule MangoWeb.Router do
     plug(:accepts, ["json"])
   end
 
+  pipeline :admin do
+    plug MangoWeb.Plugs.AdminLayout
+  end
+
   scope "/", MangoWeb do
     pipe_through([:browser, :frontend])
 
@@ -47,5 +51,14 @@ defmodule MangoWeb.Router do
     get("/orders/:order_id", OrderController, :show)
 
     resources "/tickets", TicketController, except: [:edit, :update, :delete]
+  end
+
+  scope "/admin", MangoWeb.Admin, as: :admin do
+    pipe_through [:browser, :admin]
+
+    resources "/users", UserController
+    get "/login", SessionController, :new
+    post "/sendlink", SessionController, :send_link
+    get "/magiclink", SessionController, :create
   end
 end
